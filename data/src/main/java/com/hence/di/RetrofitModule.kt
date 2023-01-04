b6c8ae8e4b820a48887d9ee50f9b095515be9eaa
Hyun Su <86879099+KimHance@users.noreply.github.com>
2023-01-04 23:08:46 +0900

    [feat]: 레트로핏 모듈 및 디스패처 모듈 생성

---
+++ data/src/main/java/com/hence/di/RetrofitModule.kt
@@ -0,0 +1,43 @@
+package com.hence.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class RetrofitModule {
+
+    @Provides
+    @Singleton
+    fun provideAfreecaRetrofit(
+        okHttpClient: OkHttpClient
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(AFREECA_BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkhttpClient(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        return OkHttpClient.Builder()
+            .addNetworkInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    companion object {
+        const val AFREECA_BASE_URL = "https://openapi.afreecatv.com/"
+    }
+}
