dda9d84671a76a2bc38a60c3f109862d11829c39
Hyun Su <86879099+KimHance@users.noreply.github.com>
2023-01-05 20:23:26 +0900

    [feat]: category tab에 적용

--- data/src/main/java/com/hence/data/repository/AfreecaRepositoryImpl.kt
+++ data/src/main/java/com/hence/data/repository/AfreecaRepositoryImpl.kt
@@ -8,6 +8,7 @@ import com.hence.data.source.paging.AfreecaPagingSource
 import com.hence.di.DispatcherModule
 import com.hence.domain.model.Broadcast
 import com.hence.domain.model.Category
+import com.hence.domain.model.CategoryType
 import com.hence.domain.repository.AfreecaRepository
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
@@ -28,7 +29,13 @@ class AfreecaRepositoryImpl @Inject constructor(
         }.flow.flowOn(dispatcherIO)
 
     override fun getCategoryList(): Flow<List<Category>> = flow {
-        emit(afreecaDataSource.getCategoryList())
+        emit(afreecaDataSource.getCategoryList().filter { category ->
+            category.name in listOf(
+                CategoryType.EAT.categoryName,
+                CategoryType.TALK.categoryName,
+                CategoryType.GAME.categoryName
+            )
+        })
     }.flowOn(dispatcherIO)
 
     companion object {
