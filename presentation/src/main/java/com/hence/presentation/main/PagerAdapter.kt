fb94bf1408b01ae333822274f95bcc43eb47ef4b
Hyun Su <86879099+KimHance@users.noreply.github.com>
2023-01-05 23:12:56 +0900

    [refact]: pager어댑터에서 bundle로 카테고리 넘버 넘겨주기

--- presentation/src/main/java/com/hence/presentation/main/PagerAdapter.kt
+++ presentation/src/main/java/com/hence/presentation/main/PagerAdapter.kt
@@ -1,27 +1,53 @@
 package com.hence.presentation.main
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.hence.domain.model.Category
 import com.hence.presentation.category.eat.EatFragment
 import com.hence.presentation.category.game.GameFragment
 import com.hence.presentation.category.talk.TalkFragment
 
-class PagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+class PagerAdapter(
+    fragmentManager: FragmentManager,
+    lifecycle: Lifecycle,
+    private val categoryList: List<Category>
+) :
     FragmentStateAdapter(fragmentManager, lifecycle) {
 
     override fun getItemCount(): Int = TAB_NUM
 
     override fun createFragment(position: Int): Fragment {
-        return when (position) {
-            0 -> TalkFragment()
-            1 -> EatFragment()
-            else -> GameFragment()
+        when (position) {
+            0 -> {
+                val fragment = TalkFragment()
+                fragment.arguments = Bundle().apply {
+                    putString(ARG_CATEGORY, categoryList[position].number)
+                }
+                return fragment
+            }
+            1 -> {
+                val fragment = EatFragment()
+                fragment.arguments = Bundle().apply {
+                    putString(ARG_CATEGORY, categoryList[position].number)
+                }
+                return fragment
+            }
+
+            else -> {
+                val fragment = GameFragment()
+                fragment.arguments = Bundle().apply {
+                    putString(ARG_CATEGORY, categoryList[position].number)
+                }
+                return fragment
+            }
         }
     }
 
     companion object {
         const val TAB_NUM = 3
+        const val ARG_CATEGORY = "key_category"
     }
 }
