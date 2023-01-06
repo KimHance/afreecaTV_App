1ae1060c2ce972688653d7889e44a4dff1b45bbb
Hyun Su <86879099+KimHance@users.noreply.github.com>
2023-01-06 17:59:52 +0900

    [feat]: detail화면 뷰 구현

---
+++ presentation/src/main/java/com/hence/presentation/detail/DetailFragment.kt
@@ -0,0 +1,8 @@
+package com.hence.presentation.detail
+
+import com.hence.presentation.R
+import com.hence.presentation.base.BaseFragment
+import com.hence.presentation.databinding.FragmentDetailBinding
+
+class DetailFragment : BaseFragment<FragmentDetailBinding>(R.layout.fragment_detail) {
+}
