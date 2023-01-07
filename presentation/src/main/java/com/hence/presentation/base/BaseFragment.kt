4b90bd51bbd8c51c659cb70ef3a21f63f0d57833
Hyun Su <86879099+KimHance@users.noreply.github.com>
2023-01-08 03:07:24 +0900

    [fix]: BaseFragment 수정

--- presentation/src/main/java/com/hence/presentation/base/BaseFragment.kt
+++ presentation/src/main/java/com/hence/presentation/base/BaseFragment.kt
@@ -1,7 +1,9 @@
 package com.hence.presentation.base
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
@@ -9,21 +11,27 @@ import androidx.fragment.app.Fragment
 
 abstract class BaseFragment<VB : ViewDataBinding>(
     @LayoutRes private val layoutId: Int
-) : Fragment(layoutId) {
+) : Fragment() {
     private var _binding: VB? = null
     protected val binding: VB
         get() = requireNotNull(_binding)
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _binding = DataBindingUtil.bind(view)
-        with(binding) {
-            lifecycleOwner = viewLifecycleOwner
-        }
+        binding.lifecycleOwner = viewLifecycleOwner
     }
 
     override fun onDestroyView() {
-        super.onDestroyView()
         _binding = null
+        super.onDestroyView()
     }
 }
